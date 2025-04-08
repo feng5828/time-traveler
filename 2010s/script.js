dd76291f0d1679ac2604b7b2276239c6d7a39bf3
hwen16 <haohanwen16@gmail.com>
2025-04-08 09:05:03 -0400

    5

--- 2010s/script.js
+++ 2010s/script.js
@@ -1,4 +1,23 @@
-function toggleContent() {
-	let content = document.querySelector("#content");
-	content.classList.toggle('hidden');
+function loadSection(sectionNumber){
+	const all = document.querySelectorAll('.section');
+  	all.forEach(section => {
+    section.style.display = 'none';
+    });
+
+	const loadedSection = document.getElementById('section'+ sectionNumber);
+	loadedSection.style.display = 'block';
+
+	const colors = [
+        '#ebf5ce', // placeholder
+        '#87e68c', // section 1
+        '#4fd19b', // section 2
+        '#68dede', // section 3
+        '#65c0db', // section 4
+        '#879fd6', // section 5
+        '#aba6e3'  // section 6
+    ];
+
+    document.body.style.backgroundColor = colors[sectionNumber] ;
+
+	
 }
